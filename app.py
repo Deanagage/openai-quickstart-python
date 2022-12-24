7ec4a5f7ac2813764fcfc5282c1d35721f012790
Dean <deano2440@gmail.com>
2022-12-24 12:08:48 +0100

    nicer

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["Animal"]
+        animal = request.form["animal"]
         response = openai.Completion.create(
-            model="text-davinci-002",
+            model="text-davinci-003",
             prompt=generate_prompt(animal),
             temperature=0.6,
         )
